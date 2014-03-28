4a20a7bc988bcb3e4b64e3f82aa2e3d849737c9f
Alex Nault <nault.alex@gmail.com>
2014-03-27 21:39:56 -0400

    Begining of multi-threading

---
+++ TownSimulator/Villagers/Villager.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using TownSimulator.Items;
+
+namespace TownSimulator.Villagers
+{
+    abstract class Villager : TileEngine.GameObject
+    {
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public string FullName { get { return FirstName + " " + LastName; } }
+        public uint Age { get; private set; }
+
+        public uint Hunger { get; private set; }
+        public uint Thirst { get; private set; }
+        public uint Satisfaction { get { return 100 - ((Hunger + Thirst) / 2); } }
+
+        public Thread thread { get; private set; }
+
+        public Item ItemHolding { get; set; }
+
+        protected Town HomeTown { get; set; }
+        
+        public Villager(string firstname, string lastname, Town hometown)
+        {
+            FirstName = firstname;
+            LastName = lastname;
+            Age = (uint) new Random().Next(18, 50);
+
+            Hunger = 0;
+            Thirst = 0;
+
+            HomeTown = hometown;
+
+            thread = new Thread(new ThreadStart(Start));
+            thread.Priority = ThreadPriority.Lowest;
+            thread.Start();
+        }
+
+        virtual protected void Start()
+        {
+            Run();
+        }
+
+        abstract protected void Run();
+    }
+}
---
+++ TownSimulator/Villagers/Woodcutter.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace TownSimulator.Villagers
+{
+    class Woodcutter : Villager
+    {
+
+        public Woodcutter(string firstname, string lastname, Town hometown)
+            : base(firstname, lastname, hometown)
+        {
+            Position = new Point(10, 10);
+        }
+
+        protected override void Run()
+        {
+            while (true) // Kill Villager
+            {
+                //Semaphore s = new Semaphore(0, 1);
+                Thread.Sleep(1000);
+                //s.WaitOne();
+            }
+        }
+    }
+}
